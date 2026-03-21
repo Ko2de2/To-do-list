fd8ef7ec35d12e88bc03c97d5e41a7e3614a44b3
Ko2de2 <lingaming14@gmail.com>
2026-03-21 11:59:00 +0700

    Basic to do list with name

---
+++ Storages/1.py
@@ -0,0 +1,27 @@
+# python 1.py
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>" 
+
+if __name__ == "__main__":
+    app.run(debug=True)
