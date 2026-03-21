4c74473f520d46636a46fdb56c3e29ffd0819206
Ko2de2 <lingaming14@gmail.com>
2026-03-21 17:11:32 +0700

    add deadline and explanation feature

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ print("Database will be created at:", os.path.join(os.getcwd(), 'test.db'))
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    deadline = db.Column(db.String(50), nullable=True)
+    explanation = db.Column(db.Text, nullable=True)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -23,23 +25,27 @@ class Todo(db.Model):
 @app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == "POST":
+        # get the information based in index.html
         task_content = request.form["content"]
-        new_task = Todo(content=task_content)
-        task_deadline = request.form["deadline"]
-        new_deadline = Todo(deadlie=task_deadline)
+        task_deadline = request.form["deadline"]   
         task_explanation = request.form["explanation"]
-        new_explanation = Todo(explanation=task_explanation)
+        
+        # put the information on Todo class/models
+        new_task = Todo(
+            content=task_content,
+            deadline=task_deadline,
+            explanation=task_explanation
+        )
 
         try:
             db.session.add(new_task)
-            db.session.add(new_deadline)
-            db.session.add(new_explanation)
             db.session.commit()
             return redirect("/")
         except:
             return "There was an issue while adding your task"
         
     else:
+        # where the tasks come from
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
@@ -60,7 +66,14 @@ def update(id):
     
     if request.method == "POST":
         task_content = request.form["content"]
-        new_task = Todo(content=task_content)
+        task_deadline = request.form["deadline"]   
+        task_explanation = request.form["explanation"]
+
+        new_task = Todo(
+            content=task_content,
+            deadline=task_deadline,
+            explanation=task_explanation
+        )
 
         try:
             db.session.delete(task_to_delete)
@@ -73,9 +86,11 @@ def update(id):
         
     else:
         task = Todo.query.get_or_404(id)
-        return render_template('update.html', task=task)           
+        return render_template('update.html', task=task)          
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
